7e3d2a0999861006697c28b2a1c12c4472a2faa9
Trevor Seitz <trevinla@gmail.com>
2022-04-29 10:13:22 -0400

    Basic Structure

--- foreverClipboard.py
+++ foreverClipboard.py
@@ -2,4 +2,17 @@ import sys
 import pyperclip
 import json
 
-data = clipboard.paste()
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    
+    if command == "save":
+        print("Save")
+    elif command == "load":
+        print("Load")
+    elif command == "list":
+        print("List")
+    else:
+        print("Unknown command")
+    
+else:
+    print("Please include only one command!")
