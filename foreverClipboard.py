1fdf826af2b2efa2903d984c058097d53946374f
Trevor Seitz <trevinla@gmail.com>
2022-04-29 10:21:38 -0400

    save-load

--- foreverClipboard.py
+++ foreverClipboard.py
@@ -1,12 +1,23 @@
+from audioop import add
 import sys
 import pyperclip
 import json
+
+def save_items(filepath, data):
+    with open(filepath, "w"):
+        json.dump(data. f)
+
+def load_items(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
     
     if command == "save":
         print("Save")
+        save_items("clipboard.json", )
     elif command == "load":
         print("Load")
     elif command == "list":
